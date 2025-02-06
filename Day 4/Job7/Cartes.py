32a0b52a2db92629fb0ebf65a3875c240a372a44
Hippo <hippolyte.geslain@laplateforme.io>
2025-02-06 12:03:57 +0100

    just before meal commit

---
+++ Day 4/Job7/Cartes.py
@@ -0,0 +1,4 @@
+class Cartes():
+    def __init__(self):
+        self.couleur =
+        self.valeur =
---
+++ Day 4/Job7/Jeu.py
@@ -0,0 +1,22 @@
+import random
+class Jeu():
+    def __init__(self):
+        self.running = True
+        self.paquet = 
+
+    def end_game(self):
+        print()
+    def draw_cards(self,joueur):
+        random.choice(self.paquet)
+
+    def premier_tour(self):
+        self.draw_cards(2,J)
+    def tour_du_joueur(self):
+        self.choice
+    def run(self):
+        while self.running:
+
+            self.events_boucle()
+            self.update()
+            self.afficher_jeu()
+            self.end_game()
